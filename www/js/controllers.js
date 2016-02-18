ca5bb8bc6c1d7fcc03155930b0cc4a1c578f1f6d
robert kelly <rkelly@Zipcoder09s-MacBook-Pro.local>
2016-02-18 10:23:52 -0500

    Working but not Object Oriented

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,39 +3,50 @@ angular.module('starter.controllers', [])
 .controller('SetsCtrl', function($scope, $http, $state) {
   $http.get('js/AllSets.json').then(function(resp) {
     $scope.sets = resp.data;
-    var curData = $state.params.setCode;
+    $scope.curData = $state.params.setCode;
     var set = [];
     angular.forEach($scope.sets, function(value, key) {
-      if( value.code == curData) this.push(value);
+      if( value.code == $scope.curData) this.push(value);
     }, set);
     $scope.set = set;
-    $scope.goto = function(toState, params){
-      $state.go(toState, params);
+
+    var card = [];
+    if (set.length == 0) {
+      angular.forEach($scope.sets, function(value, key) {
+        angular.forEach(value.cards, function(value2, key2) {
+          if( value2.multiverseid == $scope.curData) this.push(value2);
+        }, card);
+      }, card);
     }
-    // For JSON responses, resp.data contains the result
+    $scope.card = card;
+
+
+
+
+
   }, function(err) {
     console.error('ERR', err);
     // err.status will contain the status code
   })
 })
 
+
+/*
 .controller('CardsCtrl', function($scope, $http, $state) {
   $http.get('js/AllSets.json').then(function(resp) {
     $scope.sets = resp.data;
-    var curData = $state.params.setID;
+    $scope.curData = $state.params.setID;
     var card = [];
     angular.forEach($scope.sets, function(value, key) {
       angular.forEach(value.cards, function(value2, key2) {
-        if( value2.multiverseid == curData) this.push(value2);
+        if( value2.multiverseid == $scope.curData) this.push(value2);
       }, card);
     }, card);
     $scope.card = card;
-
-    $
     console.log($scope.card);
   })
 })
-
+*/
 
 
 
